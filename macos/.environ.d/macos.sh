0ccffd31bcf7b6512f7ef17ec7e02c4d17f6187f
eeowaa <7460185+eeowaa@users.noreply.github.com>
2021-06-14 12:20:35 -0600

    Add workaround for man(1) issues in macOS

---
+++ macos/.environ.d/macos.sh
@@ -0,0 +1,4 @@
+# https://apple.stackexchange.com/questions/374025/errors-from-whatis-command-unable-to-rebuild-database-with-makewhatis
+alias apropos='apropos 2>/dev/null'
+alias whatis='whatis 2>/dev/null'
+alias man='man 2>/dev/null'
