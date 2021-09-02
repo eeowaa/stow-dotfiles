efd2ba7225afd977464fea0f5c2574c1981c8ee3
eeowaa <7460185+eeowaa@users.noreply.github.com>
2021-09-02 10:14:38 -0600

    Only set BROWSER variable in Linux

--- firefox/.profile.d/firefox.sh
+++ firefox/.profile.d/firefox.sh
@@ -1,6 +1,9 @@
-# Set the default browser to firefox at runlevels 4-5
-case `runlevel | cut -d' ' -f2` in
-[45])
-    BROWSER=firefox
-    export BROWSER
+case `uname -s` in
+Linux)
+    # Set the default browser to firefox at runlevels 4-5
+    case `runlevel | cut -d' ' -f2` in
+    [45])
+        BROWSER=firefox
+        export BROWSER
+    esac
 esac
--- lynx/.profile.d/lynx.sh
+++ lynx/.profile.d/lynx.sh
@@ -1,9 +1,12 @@
 LYNX_CFG=$XDG_CONFIG_HOME/lynx/lynx.cfg
 export LYNX_CFG
 
-# Set the default browser to lynx at runlevels 1-3
-case `runlevel | cut -d' ' -f2` in
-[123])
-    BROWSER=lynx
-    export BROWSER
+case `uname -s` in
+Linux)
+    # Set the default browser to lynx at runlevels 1-3
+    case `runlevel | cut -d' ' -f2` in
+    [123])
+        BROWSER=lynx
+        export BROWSER
+    esac
 esac
