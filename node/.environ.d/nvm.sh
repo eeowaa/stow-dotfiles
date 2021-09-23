555bba77136ce9e09fc3f0830a7b604c8cb698f0
eeowaa <7460185+eeowaa@users.noreply.github.com>
2021-09-23 10:58:57 -0600

    Lazy-load NVM environment

--- node/.environ.d/nvm.sh
+++ node/.environ.d/nvm.sh
@@ -1,2 +1,17 @@
-[ -f "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-[ -f "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
+# The first time any one of these commands are run in the current shell, the
+# `nvm_init` function will run and lazy-load the NVM environment
+alias nvm='nvm_init nvm'
+alias npm='nvm_init npm'
+alias node='nvm_init node'
+
+nvm_init() {
+    # Remove command hooks for lazy-loading
+    unalias nvm npm node
+
+    # Load the NVM environment (expensive operation)
+    [ -f "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+    [ -f "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
+
+    # Execute original `nvm`, `npm`, or `node` command
+    "$@"
+}
