b4852cd28a3cd6bfc9f00408effa9167d20ab3e3
eeowaa <7460185+eeowaa@users.noreply.github.com>
2021-09-23 19:24:13 -0600

    Ensure that nvm updates PATH during shell initialization

--- node/.profile.d/nvm.sh
+++ node/.profile.d/nvm.sh
@@ -1,2 +1,6 @@
 NVM_DIR=$XDG_CONFIG_HOME/nvm
 export NVM_DIR
+
+# Insert "$NVM_DIR/versions/node/.../bin" at the front of PATH so that programs
+# installed with `nvm` can be found before lazy-loading the rest of NVM
+[ -f "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
