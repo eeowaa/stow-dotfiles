d57a331ffb8e4c693fb26c6a92ba4e6e2889ead1
eeowaa <7460185+eeowaa@users.noreply.github.com>
2021-10-14 09:16:00 -0600

    Add aws_account_name function

--- aws/.environ.d/aws.sh
+++ aws/.environ.d/aws.sh
@@ -13,6 +13,9 @@ aws() {
     login)
         shift
         aws_login ${1+"$@"} ;;
+    account-name)
+        shift
+        aws_account_name ${1+"$@"} ;;
     *)
         command aws ${1+"$@"} ;;
     esac
@@ -22,8 +25,8 @@ aws() {
 aws_login() {
     # Interactively obtain a profile if one wasn't supplied
     if [ $# -eq 0 ]
-    then profile=`command aws configure list-profiles | fzf`
-    else profile=$*
+    then local profile=`command aws configure list-profiles | fzf`
+    else local profile=$*
     fi
     [ "X$profile" = X ] && return 1
 
@@ -40,4 +43,22 @@ aws_login() {
 
         { set +x; } 2>/dev/null
     }
+}
+
+aws_account_name() {
+    [ $# -eq 1 ] || {
+        echo >&2 'Usage: aws account-name ACCOUNT_ID'
+        return 1
+    }
+    local account_id=$1 config_file=$AWS_CONFIG_FILE
+    [ -f "$config_file" ] || config_file=$HOME/.aws/config
+
+    # XXX Assumptions:
+    # 1. `sso_account_id` is always used to store account IDs
+    # 2. The format is always `sso_account_id = <ID>`, spaces included
+    # 3. The profile format is always `[profile <account-name>-<role>]`
+    tac "$config_file" | awk '
+        $1 == "sso_account_id" && $3 == ID { x = 1 }
+        x && /\[profile / { print $2; exit }
+    ' ID="$account_id" | sed 's/\(.*\)-.*/\1/'
 }
