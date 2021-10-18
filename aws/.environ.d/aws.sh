a0aa6809f09f0edec090c192fcf094c4688967d6
eeowaa <7460185+eeowaa@users.noreply.github.com>
2021-10-18 16:19:47 -0600

    Add prompt, whoami, and hostname aws subcommands

--- aws/.environ.d/aws.sh
+++ aws/.environ.d/aws.sh
@@ -9,17 +9,21 @@ fi
 
 # Thin wrapper for aws command
 aws() {
-    case $1 in
-    login)
+    local subcommand=$1
+    case $subcommand in
+    prompt|whoami|hostname|login|account-name)
         shift
-        aws_login ${1+"$@"} ;;
-    account-name)
-        shift
-        aws_account_name ${1+"$@"} ;;
+        eval aws_`echo $subcommand | tr - _` ${1+"$@"} ;;
     *)
         command aws ${1+"$@"} ;;
     esac
 }
+
+alias aws_prompt='command aws --cli-auto-prompt'
+alias aws_whoami='command aws sts get-caller-identity'
+
+# For some reason, `aws iam list-account-aliases` doesn't work (should it?)
+alias aws_hostname='aws account-name `aws whoami --query Account --output text`'
 
 # Wrapper for `aws sso login`
 aws_login() {
@@ -45,6 +49,13 @@ aws_login() {
     }
 }
 
+# Translate an exact account ID into its name (as stored in the global AWS
+# config file). This command relies on the following assumptions and conventions:
+#
+# 1. `sso_account_id` is always used to store account IDs
+# 2. The format is always `sso_account_id = <ID>`, spaces included
+# 3. The profile format is always `[profile <account-name>-<role>]`
+#
 aws_account_name() {
     [ $# -eq 1 ] || {
         echo >&2 'Usage: aws account-name ACCOUNT_ID'
@@ -53,10 +64,6 @@ aws_account_name() {
     local account_id=$1 config_file=$AWS_CONFIG_FILE
     [ -f "$config_file" ] || config_file=$HOME/.aws/config
 
-    # XXX Assumptions:
-    # 1. `sso_account_id` is always used to store account IDs
-    # 2. The format is always `sso_account_id = <ID>`, spaces included
-    # 3. The profile format is always `[profile <account-name>-<role>]`
     tac "$config_file" | awk '
         $1 == "sso_account_id" && $3 == ID { x = 1 }
         x && /\[profile / { print $2; exit }
