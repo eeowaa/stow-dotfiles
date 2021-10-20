8d87c9304bc867f83de35cefa14476938d3372fe
eeowaa <7460185+eeowaa@users.noreply.github.com>
2021-10-19 20:31:32 -0600

    Add persistent logging to AWS CLI

--- aws/.environ.d/aws.sh
+++ aws/.environ.d/aws.sh
@@ -7,7 +7,7 @@ else
     echo >&2 'AWS CLI completion not available for current shell'
 fi
 
-# Thin wrapper for aws command
+# Wrapper for `aws` with additional subcommands and per-profile logging
 aws() {
     local subcommand=$1
     case $subcommand in
@@ -15,12 +15,101 @@ aws() {
         shift
         eval aws_`echo $subcommand | tr - _` ${1+"$@"} ;;
     *)
-        command aws ${1+"$@"} ;;
+        # Determine where to log the command
+        local logfile=$XDG_CACHE_HOME/aws/log/${AWS_PROFILE:-'default'}.log
+
+        # Log the current time
+        date +'# %D %T' >>"$logfile"
+
+        # Determine if we have autoprompting enabled for this command
+        local autoprompt
+        if env | grep -iq 'aws_cli_auto_prompt=on'
+        then
+            autoprompt=on
+        else
+            for arg
+            do  [ "X$arg" = 'X--cli-auto-prompt' ] && {
+                    autoprompt=on
+                    break
+                }
+            done
+        fi
+
+        # No autoprompting
+        if [ "X$autoprompt" = X ]
+        then
+            # Log the command line with shell-quoted arguments
+            {
+                set -x
+                : QUOTED_ARGS ${1+"$@"}
+                set +x
+            } 2>&1 | sed -n -e 's/.*: QUOTED_ARGS /> aws /p' >>"$logfile"
+
+            # Run the command and log its output
+            command aws ${1+"$@"} | tee -a "$logfile"
+
+            # Append an extra newline
+            echo >>"$logfile"
+
+        # Autoprompting enabled
+        else
+            # We need to use `expect` because autoprompting requires STDOUT to be
+            # connected to a terminal in order to provide a good user experience,
+            # which is kind of the whole point of the autoprompting interface
+            local tmpfile=`mktemp`
+            expect -c "
+# Open a temporary file for writing
+set log [open \"$tmpfile\" w]
+
+# Run our command, hoping for the best with shell quoting
+spawn command aws $*
+
+# Wait for two consecutive carriage returns from the user
+interact \"\r\r\" return
+
+# Then send those carriage returns to the program
+send \"\r\"
+send \"\r\"
+
+# Capture the command prompt to show up
+expect -re \"(> aws .*)\n\" {
+    append output \$expect_out(0,string)
+}
+
+# Capture all of the command output
+expect {
+    \"\n\" {
+        append output \$expect_out(buffer)
+        exp_continue
+    }
+    eof {
+        append output \$expect_out(buffer)
+    }
+}
+
+# Write to our temporary file
+puts \$log \$output" 
+
+            # Remove control codes from the output
+            # Magic courtesy of <https://unix.stackexchange.com/a/18979>
+            perl -pe '
+s/ \e[ #%()*+\-.\/]. |
+    \r | # Remove extra carriage returns also
+    (?:\e\[|\x9b) [ -?]* [@-~] | # CSI ... Cmd
+    (?:\e\]|\x9d) .*? (?:\e\\|[\a\x9c]) | # OSC ... (ST|BEL)
+    (?:\e[P^_]|[\x90\x9e\x9f]) .*? (?:\e\\|\x9c) | # (DCS|PM|APC) ... ST
+    \e.|[\x80-\x9f] //xg;
+1 while s/[^\b][\b]//g;  # remove all non-backspace followed by backspace
+' "$tmpfile" >>"$logfile"
+
+            # Remove our temporary file
+            rm "$tmpfile"
+        fi ;;
     esac
 }
 
-alias aws_prompt='command aws --cli-auto-prompt'
-alias aws_whoami='command aws sts get-caller-identity'
+alias aws_prompt='aws --cli-auto-prompt'
+alias aws_whoami='aws sts get-caller-identity'
 
 # For some reason, `aws iam list-account-aliases` doesn't work (should it?)
 alias aws_hostname='aws account-name `aws whoami --query Account --output text`'
@@ -35,17 +124,15 @@ aws_login() {
     [ "X$profile" = X ] && return 1
 
     # Use the system web browser to supply credentials
-    BROWSER= aws sso login --profile "$profile" && {
-        # Let the user see what is going on
-        set -x
+    BROWSER= AWS_PROFILE="$profile" aws sso login && {
 
         # Bypass the need for the --profile flag upon subsequent AWS CLI invocations
+        echo "+ export AWS_PROFILE=$profile"
         export AWS_PROFILE=$profile
 
         # Print debugging information
-        command aws sts get-caller-identity
-
-        { set +x; } 2>/dev/null
+        echo "+ aws sts get-caller-identity"
+        aws sts get-caller-identity
     }
 }
 
