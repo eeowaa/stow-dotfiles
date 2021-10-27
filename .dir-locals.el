0324bf2a13130d701aabfbea3b8a5d91057c202f
eeowaa <7460185+eeowaa@users.noreply.github.com>
2021-10-27 10:54:47 -0600

    Add projectile command definitions

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,13 @@
-((org-mode (org-footnote-section . "Reference")
-           (org-footnote-auto-label . t)
-           (org-footnote-auto-adjust . t)))
+((nil . ((projectile-project-configure-cmd . "EDITOR='emacsclient -n -a false' make configure")
+         (projectile-project-compilation-cmd . "make submodules")
+         (projectile-project-test-cmd . "make test")
+         (projectile-project-run-cmd . "make")
+         (projectile-project-install-cmd . "make") ;; same as above
+         (projectile-project-package-cmd . "make dist")))
 
-;; NOTE: In order to maintian proper spacing of Org footnotes, you must redefine
-;; the `org-footnote-sort' function to *not* insert a leading "\n" before new
-;; footnote definitions.
+ ;; NOTE: In order to maintian proper spacing of Org footnotes, you must redefine
+ ;; the `org-footnote-sort' function to *not* insert a leading "\n" before new
+ ;; footnote definitions.
+ (org-mode . ((org-footnote-section . "Reference")
+              (org-footnote-auto-label . t)
+              (org-footnote-auto-adjust . t))))
