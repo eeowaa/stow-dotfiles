01299cc4e0096e603dda572fc4ac74f0e8ab2b17
eeowaa <7460185+eeowaa@users.noreply.github.com>
2021-11-02 22:30:34 -0600

    Add comments to jump.sh

--- utils/.environ.d/jump.sh
+++ utils/.environ.d/jump.sh
@@ -1,6 +1,10 @@
 ## Requires: fd fzf
+
+# An alias allows us to interpolate $JUMP_OPTIONS literally, while expanding
+# and quoting "$@" appropriately when passed as an argument to the command line
 alias _jumpdirs="\
 fd --hidden --type directory --exclude '.git*' $JUMP_OPTIONS . 2>/dev/null"
+
 jump() {
     case $1 in
     -l) [ "X$JUMPLIST" = X ] || echo "X$JUMPLIST" | cut -c2- | tr : '\n'
@@ -15,16 +19,21 @@ jump() {
         ;;
     *)
         [ $# -eq 0 ] && {
-            local toggle
+            # Enable word splitting in zsh
+            local nosplit
             [ "$ZSH_VERSION" ] && [[ ! -o shwordsplit ]] && {
                 setopt shwordsplit
-                toggle=X
+                nosplit=X
             }
+
+            # Split colon-delimited JUMP_PATH and assign to argument list
             local OLD_IFS=$IFS
             IFS=:
             set -- ${JUMP_PATH:-$HOME}
+
+            # Restore IFS and setting for word splitting
             IFS=$OLD_IFS
-            [ "$toggle" ] && unsetopt shwordsplit
+            [ "$nosplit" ] && unsetopt shwordsplit
         }
         JUMPDIR=`_jumpdirs "$@" | fzf`
         [ "X$JUMPDIR" = X ] && return 1
