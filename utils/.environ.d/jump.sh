475350fb26f566b126befe96829d32422d6769a4
eeowaa <7460185+eeowaa@users.noreply.github.com>
2021-11-02 22:11:39 -0600

    Fix jump command

--- utils/.environ.d/jump.sh
+++ utils/.environ.d/jump.sh
@@ -1,4 +1,6 @@
 ## Requires: fd fzf
+alias _jumpdirs="\
+fd --hidden --type directory --exclude '.git*' $JUMP_OPTIONS . 2>/dev/null"
 jump() {
     case $1 in
     -l) [ "X$JUMPLIST" = X ] || echo "X$JUMPLIST" | cut -c2- | tr : '\n'
@@ -12,7 +14,19 @@ jump() {
         JUMPLIST=`echo "$JUMPLIST" | sed "s/^[^:]*:\{0,1\}//"`
         ;;
     *)
-        JUMPDIR=`eval "fd --hidden --type directory $JUMP_OPTIONS . '${@:-$HOME}'" | fzf`
+        [ $# -eq 0 ] && {
+            local toggle
+            [ "$ZSH_VERSION" ] && [[ ! -o shwordsplit ]] && {
+                setopt shwordsplit
+                toggle=X
+            }
+            local OLD_IFS=$IFS
+            IFS=:
+            set -- ${JUMP_PATH:-$HOME}
+            IFS=$OLD_IFS
+            [ "$toggle" ] && unsetopt shwordsplit
+        }
+        JUMPDIR=`_jumpdirs "$@" | fzf`
         [ "X$JUMPDIR" = X ] && return 1
         JUMPLIST=$JUMPDIR${JUMPLIST:+:}$JUMPLIST
         ;;
