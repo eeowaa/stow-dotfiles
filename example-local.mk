01e55f01aca5361600a13545cb0f0f924dfdbea7
eeowaa <7460185+eeowaa@users.noreply.github.com>
2021-11-15 03:15:08 -0700

    Untangle macos and ssh agent

--- example-local.mk
+++ example-local.mk
@@ -1,5 +1,5 @@
 # Override packages
 packages := \
-X11 aws bash cygwin direnv docker doom dotnet emacs firefox gimp git \
-gnupg go i3 info irc ldap less linux locate lynx macos mintty node openssl \
-org perl python shell ssh-agent tmux units utils vim wget work xterm zsh
+aws bash cygwin direnv docker doom dotnet emacs firefox gimp git gnupg \
+go i3 info irc ldap less linux locate lynx macos mintty node openssl org \
+perl python rust shell ssh tmux units utils vim wget work X11 xterm zsh
