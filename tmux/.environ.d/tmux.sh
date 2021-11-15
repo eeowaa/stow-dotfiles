6066ed46801ecd7d61104810b1cd9d75e58ab9d7
eeowaa <7460185+eeowaa@users.noreply.github.com>
2021-11-15 02:44:40 -0700

    Fix tmux colors in linux terminal

--- tmux/.environ.d/tmux.sh
+++ tmux/.environ.d/tmux.sh
@@ -1,1 +1,7 @@
-alias tmux='tmux -f "$XDG_CONFIG_HOME/tmux/tmux.conf"'
+## Requires: tput
+tmux() {
+    if [ "`tput colors`" -lt 256 ]
+    then command tmux -f "$XDG_CONFIG_HOME/tmux/tmux.conf" ${1+"$@"}
+    else command tmux -f "$XDG_CONFIG_HOME/tmux/tmux.conf" -2 ${1+"$@"}
+    fi
+}
