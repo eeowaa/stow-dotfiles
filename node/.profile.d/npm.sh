207d7d616b439260b288a616befb0fcabe867e7b
eeowaa <7460185+eeowaa@users.noreply.github.com>
2022-02-04 15:03:18 -0700

    Refactor npm config
    - Remove XDG configuration
    - Use environment variables in ~/.profile.d/npm.sh for
      machine-independent configuration
    - Use ~/.npmrc for machine-specific configuration

--- node/.profile.d/npm.sh
+++ node/.profile.d/npm.sh
@@ -1,2 +1,13 @@
-NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
-export NPM_CONFIG_USERCONFIG
+# Reference: https://manpages.org/npm
+#
+# 1. Specify machine-independent user configuration as environment variables
+#    in `NPM_CONFIG_KEY=value' format, where KEY is any key that would be found
+#    in an npmrc file. For example:
+#
+#    NPM_CONFIG_CACHE=$XDG_CACHE_HOME/npm
+#    NPM_CONFIG_TMP=$XDG_RUNTIME_DIR/npm
+#    NPM_CONFIG_INIT_MODULE=$XDG_CONFIG_HOME/npm/config/npm-init.js
+#    export NPM_CONFIG_CACHE NPM_CONFIG_TMP NPM_CONFIG_INIT_MODULE
+#
+# 2. Put machine-specific user configuration in ~/.npmrc, which is ignored
+#    by stow-dotfiles.
