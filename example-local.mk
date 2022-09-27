c38b0f462c942621cf207ec2b9c2a547910a2ad4
eeowaa <7460185+eeowaa@users.noreply.github.com>
2022-09-27 16:41:52 -0600

    Add lua package

--- example-local.mk
+++ example-local.mk
@@ -2,5 +2,5 @@
 packages := \
 ansible aws az bash bat bw cygwin direnv docker doom dotnet elastic emacs \
 ffmpeg firefox flatpak gdb git go haskell i3 info irc k8s ldap less linux \
-locate lynx macos markdown mintty mongo node openssl org perl python \
+locate lua lynx macos markdown mintty mongo node openssl org perl python \
 redis rust shell ssh tmux topgrade utils vim work wsl2 X11 xterm zsh
---
+++ lua/.profile.d/luarocks.sh
@@ -0,0 +1,2 @@
+PATH=$HOME/.luarocks/bin:$PATH
+export PATH
