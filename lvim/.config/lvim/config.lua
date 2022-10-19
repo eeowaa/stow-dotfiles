96f44b918fbf03335a3c2fa2d0fdef0e8d6f1582
eeowaa <7460185+eeowaa@users.noreply.github.com>
2022-10-19 14:17:27 -0600

    Set scrolloff to 0 in lvim

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -12,6 +12,7 @@ an executable
 lvim.log.level = "warn"
 lvim.format_on_save = false
 lvim.colorscheme = "jellybeans-nvim"
+vim.opt.scrolloff = 0
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
