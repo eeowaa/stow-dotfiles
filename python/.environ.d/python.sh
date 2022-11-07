a26ff9f24d4667b233f83a6e6494d95bc18de191
eeowaa <7460185+eeowaa@users.noreply.github.com>
2022-11-07 16:05:27 -0700

    Create python virtualenv activation function

--- python/.environ.d/python.sh
+++ python/.environ.d/python.sh
@@ -1,2 +1,16 @@
+# Automatically activate a Python virtualenv
+activate() {
+    for venv in "$VIRTUAL_ENV" "`pipenv --venv 2>/dev/null`" .venv venv
+    do
+        if [ -d "$venv" ] && [ -f "$venv/bin/activate" ]
+        then
+            . "$venv/bin/activate"
+            return $?
+        fi
+    done
+    echo >&2 'Could not find virtualenv activation script'
+    return 1
+}
+
 # FIXME
 alias pyref='$BROWSER file:///usr/share/doc/python2/html/index.html'
