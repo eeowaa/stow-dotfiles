d8fe721b0cf9735b341b00cc657beb6b8791b48c
eeowaa <7460185+eeowaa@users.noreply.github.com>
2022-12-27 13:42:22 -0700

    Create "output" plugin for helm

---
+++ k8s/.local/share/helm/plugins/helm-output/output.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+# Render chart templates to "$HELM_CACHE_DIR/output"
+eval "`helm env`"
+outdir=`realpath "$HELM_CACHE_HOME/output"`
+$HELM_BIN template --output-dir "$outdir" ${1+"$@"}
+
+# Change "<repo>/<path>" to "<chartdir>/<path>" in comments
+chartdir=`pwd | sed 's,/,\\\\/,g'`
+find "$outdir" -type f | while read f
+do
+    ed "$f" >/dev/null <<EOF
+g/^# Source: [^\\/]*/s//# Source: $chartdir/
+w
+q
+EOF
+done
