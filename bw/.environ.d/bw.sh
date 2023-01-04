63da2f9512d762dfd4fbcb15aacabb335a988703
eeowaa <7460185+eeowaa@users.noreply.github.com>
2023-01-04 11:18:17 -0700

    Automatically cache bw creds

--- bw/.environ.d/bw.sh
+++ bw/.environ.d/bw.sh
@@ -3,9 +3,25 @@
 bw() { (
     set +x
     [ -d "$XDG_CACHE_HOME" ] || XDG_CACHE_HOME=$HOME/.cache
-    if [ -r "$XDG_CACHE_HOME/bw/session" ]; then
-        BW_SESSION=`cat "$XDG_CACHE_HOME/bw/session"`
+    cache=$XDG_CACHE_HOME/bw/session
+    if [ -r "$cache" ]; then
+        BW_SESSION=`cat "$cache"`
         export BW_SESSION
     fi
-    command bw ${1+"$@"}
+    case $1 in
+    login)
+        [ -f "$cache" ] || touch "$cache"
+        chmod 600 "$cache"
+        command bw ${1+"$@"} | awk '
+        match($0, /--session [a-zA-Z0-9+/=]{88}/) {
+            key = substr($0, RSTART + 10, 88)
+            print key > CACHE
+        } {
+            print
+        } END {
+            print "\n*** session key has been saved to", CACHE
+        }' CACHE="$cache" ;;
+    *)
+        command bw ${1+"$@"} ;;
+    esac
 ); }
