847524e5441e59f2784283dfeab23e2296e24fc2
eeowaa <7460185+eeowaa@users.noreply.github.com>
2023-09-11 12:50:45 -0600

    Fix BitWarden CLI session caching

--- bw/.profile.d/bw.sh
+++ bw/.profile.d/bw.sh
@@ -1,4 +1,18 @@
+# Reference:
+# - <https://community.bitwarden.com/t/best-practices-log-out-or-lock/51207>
+# - <https://bitwarden.com/help/cli/>
+
 if [ "$ZSH_VERSION" ]
 then bw completion --shell zsh >"$ZDOTDIR/site-functions/_bw"
 else echo >&2 'Bitwarden CLI completion not available for current shell'
 fi
+
+# Search BitWarden for passwords using the `getpw` utility script
+case $GETPW_BACKENDS in *bitwarden*) ;; *)
+    GETPW_BACKENDS=${GETPW_BACKENDS:+"$GETPW_BACKENDS "}bitwarden
+    export GETPW_BACKENDS ;;
+esac
+
+# Configure the `bw` wrapper script
+BW_SESSION_CACHE=$XDG_CACHE_HOME/bw/session
+export BW_SESSION_CACHE
---
+++ utils/.profile.d/ZZ-path.sh
@@ -0,0 +1,3 @@
+# Put ~/bin at the front of $PATH for global overrides
+PATH=$HOME/bin:$PATH
+export PATH
