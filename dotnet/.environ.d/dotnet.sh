189594c0264fa16b1988963aba62072d8cc40968
eeowaa <7460185+eeowaa@users.noreply.github.com>
2024-10-14 18:49:46 -0600

    Add dotnet shell completion

---
+++ dotnet/.environ.d/dotnet.sh
@@ -0,0 +1,4 @@
+# NOTE: bash completion should be automatic
+if [ "$ZSH_VERSION" ] && [ -f /usr/share/bash-completion/completions/dotnet ]
+then source /usr/share/bash-completion/completions/dotnet
+fi
