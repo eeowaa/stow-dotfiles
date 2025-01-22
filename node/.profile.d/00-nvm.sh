f79b19fe8ac12529f22c6117da5f942deec053bf
eeowaa <7460185+eeowaa@users.noreply.github.com>
2025-01-22 12:13:23 -0700

    Fix edge case that prevents PATH from being set for NVM

--- node/.profile.d/00-nvm.sh
+++ node/.profile.d/00-nvm.sh
@@ -3,4 +3,5 @@ export NVM_DIR
 
 # Insert "$NVM_DIR/versions/node/.../bin" at the front of PATH so that programs
 # installed with `nvm` can be found before lazy-loading the rest of NVM
+unalias nvm npm node
 [ -f "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
