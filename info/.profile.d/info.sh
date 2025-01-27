66b049a75f3bfc76dc36caa92cfb7a8e819464ff
eeowaa <7460185+eeowaa@users.noreply.github.com>
2025-01-27 15:39:52 -0700

    Add additional entry to INFOPATH

--- info/.profile.d/info.sh
+++ info/.profile.d/info.sh
@@ -1,2 +1,2 @@
-INFOPATH=/usr/local/share/info:/usr/local/info:/usr/share/info:/usr/info
+INFOPATH=$XDG_DATA_HOME/info:/usr/local/share/info:/usr/local/info:/usr/share/info:/usr/info
 export INFOPATH
