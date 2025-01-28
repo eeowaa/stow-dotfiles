85120d58973c96c10498525ddd455e88400882a1
eeowaa <7460185+eeowaa@users.noreply.github.com>
2025-01-28 14:47:34 -0700

    Remove shell completion for krew

--- k8s/.profile.d/krew.sh
+++ k8s/.profile.d/krew.sh
@@ -2,10 +2,3 @@
 
 PATH=${KREW_ROOT:-$HOME/.krew}/bin:$PATH
 export PATH
-
-if [ "$BASH_VERSION" ]
-then kubectl krew completion bash >"$BASH_COMPLETION_USER_DIR/completions/krew"
-elif [ "$ZSH_VERSION" ]
-then kubectl krew completion zsh >"$ZDOTDIR/site-functions/_krew"
-else echo >&2 'Krew completion not available for current shell'
-fi
