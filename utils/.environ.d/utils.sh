f17fafbda36bed6416238e000f1eb79db85307cf
eeowaa <7460185+eeowaa@users.noreply.github.com>
2025-01-30 13:29:56 -0700

    Add open function to utils.sh

--- utils/.environ.d/utils.sh
+++ utils/.environ.d/utils.sh
@@ -1,1 +1,14 @@
 alias a='acronym'
+open() {
+    case `uname -s` in
+    Linux)
+        xdg-open "$1" ;;
+    Darwin)
+        open "$1" ;;
+    CYGWIN_NT*)
+        cygstart -o "$1" ;;
+    MINGW*|MSYS*)
+        start '' "$1" ;;
+    *)  ${BROWSER:?} "$1" ;;
+    esac
+}
