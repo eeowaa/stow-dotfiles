2f9366e6bb18f4dce184fbbcaf12940c70b9887d
eeowaa <7460185+eeowaa@users.noreply.github.com>
2026-06-05 15:54:52 -0600

    Fix open utility for WSL

--- utils/.environ.d/utils.sh
+++ utils/.environ.d/utils.sh
@@ -1,14 +1,20 @@
 alias a='acronym'
 open() {
-    case `uname -s` in
-    Linux)
-        xdg-open "$1" ;;
-    Darwin)
-        open "$1" ;;
-    CYGWIN_NT*)
-        cygstart -o "$1" ;;
-    MINGW*|MSYS*)
-        start '' "$1" ;;
-    *)  ${BROWSER:?} "$1" ;;
+    case "$(uname -r)" in
+    *-Microsoft|*-microsoft-standard-WSL2)
+        if [ -e "$1" ]; then set -- "$(wslpath -w "$1")"; fi
+        powershell.exe -NoProfile Start-Process "$1" ;;
+    *)
+        case "$(uname -s)" in
+        Linux)
+            xdg-open "$1" ;;
+        Darwin)
+            open "$1" ;;
+        CYGWIN_NT*)
+            cygstart -o "$1" ;;
+        MINGW*|MSYS*)
+            start '' "$1" ;;
+        *)  ${BROWSER:?} "$1" ;;
+        esac ;;
     esac
 }
