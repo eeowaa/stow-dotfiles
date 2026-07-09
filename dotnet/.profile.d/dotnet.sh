c3edc41d0ea263523dc213d74e8871e0be943abf
eeowaa <7460185+eeowaa@users.noreply.github.com>
2026-07-08 20:22:24 -0600

    Add ~/.dotnet/tools to PATH

--- dotnet/.profile.d/dotnet.sh
+++ dotnet/.profile.d/dotnet.sh
@@ -1,3 +1,4 @@
 # No more Mono! .NET Core practically absorbed it in 2020
+PATH=$PATH:$HOME/.dotnet/tools
 DOTNET_CLI_TELEMETRY_OPTOUT=1
-export DOTNET_CLI_TELEMETRY_OPTOUT
+export PATH DOTNET_CLI_TELEMETRY_OPTOUT
